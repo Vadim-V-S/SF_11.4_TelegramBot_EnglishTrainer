194c4edd325d48395a378a2b0208bfb37adc8080
Vadim-V-S <vaddimsav@gmail.com>
2021-06-23 22:38:00 +0500

    Добавьте файлы проекта.

---
+++ SF_11.4_TelegramBot_EnglishTrainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SF_11._4_TelegramBot_EnglishTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
